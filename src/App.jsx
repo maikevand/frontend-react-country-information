95c909cbf0dfefeb08f3c2468c5ff093d55e3a52
maike1983 <Maikevandartel@outlook.com>
2026-05-13 10:34:59 +0200

    1: button gemaakt die info in console toont

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css';
+import axios from "axios"
 
 function App() {
 
+    async function fetchData () {
+        try {
+            const result = await axios.get("https://restcountries.com/v3.1/all?fields=name,flags,population");
+            console.log(result);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     return (
         <>
-            Maak je applicatie hier!
+            <button onClick={fetchData}>Toont landen info</button>
+            <p>hier komt het resultaat</p>
         </>
     )
 }
