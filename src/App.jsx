a5ceff26504726d5f82e6f27d990a380a1ca861a
maike1983 <Maikevandartel@outlook.com>
2026-05-13 16:26:35 +0200

    t/m 1.12 alle landen gesorteerd, zonder styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
     async function fetchData() {
         try {
             const result = await axios.get("https://restcountries.com/v3.1/all?fields=name,flags,population,continents");
+
+            result.data.sort((a, b) => a.population - b.population)
             setCountries(result.data);
             // console.log(result.data);
             // setCountryName(result.data[0].name.common)
