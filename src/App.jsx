3d39c6ecf338f53d8d5c181b8e1af66fad5a47e7
maike1983 <Maikevandartel@outlook.com>
2026-05-13 20:06:07 +0200

    t/m 2.4 info van 1 land in console weergegeven

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,27 @@ import getContinentColor from "./helpers/getContinentColor.js";
 import worldMap from "./assets/world_map.png"
 
 function App() {
-    const [countries, setCountries] = useState([]);
+    const [allCountries, setAllCountries] = useState([]);
+    const [countryByName, setCountryByName] = useState("");
 
-    async function fetchData() {
+    async function fetchAllCountries() {
         try {
             const result = await axios.get("https://restcountries.com/v3.1/all?fields=name,flags,population,continents");
-
+            console.log(result.data[1].capital);
             result.data.sort((a, b) => a.population - b.population)
-            setCountries(result.data);
-            // console.log(result.data);
-            // setCountryName(result.data[0].name.common)
-            // console.log(result.data[0].name.common)
-            // setCountryPopulation(result.data[0].population)
-            // console.log(result.data[0].population)
-            // setContinents(result.data[0].continents)
-            // console.log(result.data[0].continents)
-            // setCountryFlag(result.data[0].flags.png)
+            setAllCountries(result.data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async function fetchCountryByName() {
+        try {
+            const result = await axios.get("https://restcountries.com/v3.1/name/netherlands");
+            console.log(result.data[1].name.common);
+            console.log(result.data[1].capital[0]);
+            console.log(result.data);
+            setCountryByName(result.data);
         } catch (e) {
             console.error(e);
         }
@@ -29,24 +34,27 @@ function App() {
     return (
         <>
             <header>
-                <img src={worldMap} alt="Wereldkaart"/>
+                <img src={worldMap} alt="World map"/>
                 <h1>World Regions</h1>
 
-                {countries.length === 0 && (
-                    <button onClick={fetchData}>Toon landen</button>
+                {allCountries.length === 0 && (
+                    <div>
+                        <button onClick={fetchAllCountries}>Show all country info</button>
+                        <button onClick={fetchCountryByName}>Search country</button>
+                    </div>
                 )}
             </header>
 
             <ul className="country-list">
-                {countries.map((country) => (
+                {allCountries.map((allCountries) => (
                         <li key={country.name.common} className="country-cards">
                             <div className="card-header">
                                 <img src={country.flags.png} alt={"Vlag van " + country.name.common}/>
-                            <span className={getContinentColor(country.continents[0])}>{country.name.common}</span>
+                                <span className={getContinentColor(allCountries.continents[0])}>{country.name.common}</span>
                             </div>
                             <p>Has a
-                            population
-                            of {country.population} people</p>
+                                population
+                                of {allCountries.population} people</p>
                         </li>
                     )
                 )
