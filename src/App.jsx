1de90df97af1e2db9b917de462177b31bf9fc633
maike1983 <Maikevandartel@outlook.com>
2026-05-14 10:17:10 +0200

    t/m 2.12 Zoekveld met GET, zonder leeg worden of error-melding

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import {useState} from "react";
 import getContinentColor from "./helpers/getContinentColor.js";
 import worldMap from "./assets/world_map.png"
 import roundingNumbers from "./helpers/roundingNumbers.js";
+import spinningGlobe from "./assets/spinning-globe.gif"
 
 function App() {
     const [allCountries, setAllCountries] = useState([]);
     const [countryByName, setCountryByName] = useState(null);
+    const [searchQuery, setSearchQuery] = useState("");
 
     async function fetchAllCountries() {
         try {
             const result = await axios.get("https://restcountries.com/v3.1/all?fields=name,flags,population,continents");
-            console.log(result.data[1].capital);
+            console.log(result.data[0].capital);
             result.data.sort((a, b) => a.population - b.population)
             setAllCountries(result.data);
         } catch (e) {
@@ -22,16 +24,22 @@ function App() {
 
     async function fetchCountryByName() {
         try {
-            const result = await axios.get("https://restcountries.com/v3.1/name/netherlands");
-            console.log(result.data[1].name.common);
-            console.log(result.data[1].capital[0]);
-            console.log(result.data[1]);
-            console.log(result.data[1]);
-            console.log(roundingNumbers(result.data[1].population));
-            setCountryByName(result.data[1]);
+            const result = await axios.get(`https://restcountries.com/v3.1/name/${searchQuery}`);
+            console.log(result.data[0].name.common);
+            console.log(result.data[0].capital[0]);
+            console.log(result.data[0]);
+            console.log(result.data[0]);
+            console.log(roundingNumbers(result.data[0].population));
+            setCountryByName(result.data[0]);
         } catch (e) {
             console.error(e);
         }
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        console.log(searchQuery);
+        await fetchCountryByName();
     }
 
     return (
@@ -41,39 +49,58 @@ function App() {
                 <h1>World Regions</h1>
 
                 {allCountries.length === 0 && (
-                    <div>
-                        <button onClick={fetchAllCountries}>Show all country info</button>
-                        <button onClick={fetchCountryByName}>Search country</button>
-                    </div>
+                    <button onClick={fetchAllCountries}>Show all country info</button>
                 )}
             </header>
-
-            <ul className="country-list">
-                {allCountries.map((country) => (
-                        <li key={country.name.common} className="country-cards">
-                            <div className="card-header">
-                                <img src={country.flags.png} alt={"Vlag van " + country.name.common}/>
-                                <span className={getContinentColor(country.continents[0])}>{country.name.common}</span>
-                            </div>
-                            <p>Has a population of {country.population} people</p>
-                        </li>
-                    )
-                )
-                }
-            </ul>
-            <li>
-                {countryByName && (
-                    <>
-                        <div>
-                            <img src={countryByName.flags.png} alt="Vlag"/>
-                            <h2>{countryByName.name.common}</h2>
-                        </div>
-                        <p>{countryByName.name.common} is situated in {countryByName.subregion} and the capital
-                            is {countryByName.capital[0]}</p>
-                        <p>It has a population of {roundingNumbers(countryByName.population)} million people and it borders with {countryByName.borders.length} neighboring countries</p>
-                    </>
-                )}
-            </li>
+            <main>
+                <section>
+                    <ul className="country-list">
+                        {allCountries.map((country) => (
+                                <li key={country.name.common} className="country-cards">
+                                    <div className="card-header">
+                                        <img src={country.flags.png} alt={"Vlag van " + country.name.common}/>
+                                        <span
+                                            className={getContinentColor(country.continents[0])}>{country.name.common}</span>
+                                    </div>
+                                    <p>Has a population of {country.population} people</p>
+                                </li>
+                            )
+                        )
+                        }
+                    </ul>
+                </section>
+                <section className="search-country">
+                    <h1>Search country information</h1>
+                    <div className="search-country-content">
+                        <img src={spinningGlobe} alt="Spinning globe"/>
+                        <form className="search-button" onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                placeholder="Bijvoorbeeld Nederland of Peru"
+                                name="search"
+                                value={searchQuery}
+                                onChange={(event) => setSearchQuery(event.target.value)}
+                            />
+                            <button type="submit">Zoek</button>
+                        </form>
+                    </div>
+                    <article>
+                        {countryByName && (
+                            <>
+                                <div>
+                                <img src={countryByName.flags.png} alt="Vlag"/>
+                                    <h2>{countryByName.name.common}</h2>
+                                </div>
+                                <p>{countryByName.name.common} is situated in {countryByName.subregion} and the capital
+                                    is {countryByName.capital[0]}</p>
+                                <p>It has a population of {roundingNumbers(countryByName.population)} million people and
+                                    it
+                                    borders with {countryByName.borders.length} neighboring countries</p>
+                            </>
+                        )}
+                    </article>
+                </section>
+            </main>
         </>
     )
 }
